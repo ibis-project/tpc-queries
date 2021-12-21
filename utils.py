dcc94b0f078df882575b3fb952697ba1da95c25b
Saul Pwanson <saul@voltrondata.com>
2021-12-21 11:26:22 -0800

    add_date correctly rollover months

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def add_date(datestr, dy=0, dm=0, dd=0):
     m = dt.month+dm
     y = dt.year+dy
     y += m // 12
-    m %= 12
+    m = ((m-1) % 12) + 1
 
     newdt = datetime.date(y, m, dt.day) + datetime.timedelta(days=dd)
     return newdt.isoformat()
