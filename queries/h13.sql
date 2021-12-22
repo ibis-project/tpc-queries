bb45ae33f20039f65c8b92ff6aac614976391538
Saul Pwanson <saul@voltrondata.com>
2021-12-21 17:01:41 -0800

    fix q13 for sqlite

--- queries/h13.sql
+++ queries/h13.sql
@@ -2,11 +2,11 @@ SELECT c_count,
        count(*) AS custdist
 FROM
   (SELECT c_custkey,
-          count(o_orderkey)
+          count(o_orderkey) AS c_count
    FROM customer
    LEFT OUTER JOIN orders ON c_custkey = o_custkey
-   AND o_comment not like ‘%special%requests%’
-   GROUP BY c_custkey)AS c_orders (c_custkey, c_count)
+   AND o_comment NOT LIKE '%special%requests%'
+   GROUP BY c_custkey)
 GROUP BY c_count
 ORDER BY custdist DESC,
          c_count DESC
