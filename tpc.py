43cdf7dbadcb9a2a15f06d48e894714cab5c93a2
Saul Pwanson <saul@voltrondata.com>
2021-12-21 17:46:32 -0800

    many runner improvements

--- tpc.py
+++ tpc.py
@@ -4,7 +4,6 @@ import glob
 import itertools
 import json
 import os
-import sys
 import time
 from pathlib import Path
 from typing import List, Dict, Any
@@ -14,12 +13,11 @@ import pandas
 import ibis
 
 
-def write_stdout(s):
-    print(s, end='')
-
-
-def write_stderr(s):
-    print(s, end='', file=sys.stderr)
+def fmt(v):
+    if isinstance(v, float):
+        return '%.03f' % v
+    else:
+        return str(v)
 
 
 def out_txt(s, outdir, fn):
@@ -47,149 +45,148 @@ def out_jsonl(rows: List[Dict[str, Any]], outdir, fn):
                 print(json.dumps(r, cls=DateEncoder), file=fp, flush=True)
 
 
-def out_benchmark(outdir, **kwargs):
-    def fmt(v):
-        if isinstance(v, float):
-            return '%.03f' % v
-        else:
-            return str(v)
+class Runner:
+    def __init__(self, interface='sqlite', backend='sqlite'):
+        self.interface = interface
+        self.backend = backend
+        self.prints = []
+        self.warns = []
+        self.errors = []
 
-    if outdir:
-        with open(Path(outdir)/'benchmarks.txt', mode='a') as fp:
-            print('  '.join(f'{k}:{fmt(v)}' for k, v in kwargs.items()), file=fp)
-        with open(Path(outdir)/'benchmarks.jsonl', mode='a') as fp:
-            print(json.dumps(kwargs), file=fp)
+    def setup(self, db='tpch.db'):
+        self.prints = []
+        self.warns = []
+        self.errors = []
 
-#        if not quiet:
-    print('  '.join(f'{k}:{fmt(v)}' for k, v in kwargs.items()))
+    def teardown(self):
+        pass
+
+    def run(self, qid, outdir=None, backend='sqlite'):
+        pass
+
+    def print(self, s):
+        self.prints.append(s.strip())
+
+    def warn(self, s):
+        self.warns.append(s.strip())
+
+    def error(self, s):
+        self.errors.append(s.strip())
+
+    def info(self):
+        return dict(interface=self.interface, backend=self.backend)
 
 
-def setup_sqlite(db='tpch.db'):
-    import sqlite3
+class SqliteRunner(Runner):
+    def setup(self, db='tpch.db'):
+        super().setup(db=db)
+        import sqlite3
 
-    con = sqlite3.connect(db)
-    con.row_factory = sqlite3.Row
+        self.con = sqlite3.connect(db)
+        self.con.row_factory = sqlite3.Row
 
-    return con
+    def run(self, qid, outdir=None, backend='sqlite'):
+        cur = self.con.cursor()
 
-
-def teardown_sqlite(con):
-    pass
-
-
-def run_sqlite(con, qid, outdir=None, backend='sqlite'):
-    cur = con.cursor()
-
-    try:
         sql = open(f'queries/{qid}.sql').read()
         t1 = time.time()
         cur.execute(sql)
         rows = cur.fetchall()
         t2 = time.time()
         rows = list(dict(r) for r in rows)
-        out_jsonl(rows, outdir, f'{qid}-{backend}.jsonl')
-        return rows, dict(elapsed_s=t2-t1, nrows=len(rows))
-    except Exception as e:
-        return [], {'error': str(e)}
+        return rows, t2-t1
+
+    def info(self):
+        import sqlite3
+        return dict(interface=self.interface,
+                    backend=f'{self.backend} {sqlite3.sqlite_version}')
 
 
-def setup_ibis(db='tpch.db', backend='sqlite'):
-    return getattr(ibis, backend).connect(db)
+class IbisRunner(Runner):
+    def setup(self, db='tpch.db'):
+        super().setup(db=db)
+        self.con = getattr(ibis, self.backend).connect(db)
+
+    def run(self, qid, outdir=None, backend='sqlite'):
+        import importlib
+        mod = importlib.import_module(f'queries.{qid}')
+        q = getattr(mod, f'tpc_{qid}')(self.con)
+
+        out_txt(repr(q), outdir, f'{qid}-{self.interface}-{self.backend}-expr.txt')
+
+        out_sql(q.compile(), outdir, f'{qid}-{self.interface}-{self.backend}-compiled.sql')
+
+        t1 = time.time()
+        rows = q.execute()
+        t2 = time.time()
+
+        return rows.to_dict('records'), t2-t1
 
 
-def teardown_ibis(con):
-    pass
+class RRunner(Runner):
+    def setup(self, db='tpch.db'):
+        super().setup(db=db)
+        os.putenv('R_LIBS_SITE', '/usr/lib/R/library')  # skip warnings
+
+        import rpy2
+        import rpy2.robjects
+        import rpy2.robjects.pandas2ri
+        import rpy2.robjects.packages as rpackages
+
+        rpy2.robjects.pandas2ri.activate()
+
+        rpy2.rinterface_lib.callbacks.consolewrite_print = self.print
+        rpy2.rinterface_lib.callbacks.consolewrite_warnerror = self.warn
+
+        pkgs = ('dplyr', 'dbplyr', 'lubridate', 'DBI', 'RSQLite')
+        names_to_install = [x for x in pkgs if not rpackages.isinstalled(x)]
+
+        if names_to_install:
+            from rpy2.robjects.vectors import StrVector
+            utils = rpackages.importr('utils')
+            utils.chooseCRANmirror(ind=1)  # select first mirror in the list
+            utils.install_packages(StrVector(names_to_install))
+
+        r = rpy2.robjects.r
+        r['source']('init.R')
+
+        self.query_dbplyr = rpy2.robjects.globalenv['query_dbplyr']
+        self.query_dplyr = rpy2.robjects.globalenv['query_dplyr']
+        self.query_sql = rpy2.robjects.globalenv['query_sql']
+
+        self.con = rpy2.robjects.globalenv['setup_sqlite'](db)
+
+    def teardown(self):
+        super().teardown()
+        import rpy2.robjects
+        rpy2.robjects.globalenv['teardown_sqlite'](self.con)
+
+    def run(self, qid, outdir=None, backend='sqlite'):
+        import rpy2.robjects
+
+        r = rpy2.robjects.r
+        fn = f'queries/{qid}.R'
+
+        if not Path(fn).exists():
+            raise FileNotFoundError(fn)
+
+        r['source'](fn)
+        func = rpy2.robjects.globalenv[f'tpc_{qid}']
+
+        sql = self.query_sql(self.con, func)[0]
+        out_sql(sql, outdir, f'{qid}-{self.interface}-{self.backend}.sql')
+
+        t1 = time.time()
+        res = rpy2.robjects.globalenv['query_'+self.interface](self.con, func)
+        t2 = time.time()
+
+        return res.to_dict('records'), t2-t1
 
 
-def run_ibis(con, qid, outdir=None, backend='sqlite'):
-    import importlib
-    mod = importlib.import_module(f'queries.{qid}')
-    q = getattr(mod, f'tpc_{qid}')(con)
-
-    out_txt(repr(q), outdir, f'{qid}-ibis-repr.txt')
-
-    out_sql(q.compile(), outdir, f'{qid}-ibis-{backend}.sql')
-
-    t1 = time.time()
-    rows = q.execute()
-    t2 = time.time()
-
-    outrows = rows.to_dict('records')
-    out_jsonl(outrows, outdir, f'{qid}-ibis-{backend}.jsonl')
-    return outrows, dict(elapsed_s=t2-t1, nrows=len(outrows))
-
-
-def setup_r(db='tpch.db', backend='sqlite', quiet=False):
-    os.putenv('R_LIBS_SITE', '/usr/lib/R/library')  # skip warnings
-
-    import rpy2
-    import rpy2.robjects
-    import rpy2.robjects.pandas2ri
-    import rpy2.robjects.packages as rpackages
-
-    rpy2.robjects.pandas2ri.activate()
-
-    if quiet:
-        rpy2.rinterface_lib.callbacks.consolewrite_print = lambda s: None
-        rpy2.rinterface_lib.callbacks.consolewrite_warnerror = lambda s: None
-    else:
-        rpy2.rinterface_lib.callbacks.consolewrite_print = write_stdout
-        rpy2.rinterface_lib.callbacks.consolewrite_warnerror = write_stderr
-
-    pkgs = ('dplyr', 'dbplyr', 'lubridate', 'DBI', 'RSQLite')
-    names_to_install = [x for x in pkgs if not rpackages.isinstalled(x)]
-
-    if names_to_install:
-        from rpy2.robjects.vectors import StrVector
-        utils = rpackages.importr('utils')
-        utils.chooseCRANmirror(ind=1)  # select the first mirror in the list
-        utils.install_packages(StrVector(names_to_install))
-
-    r = rpy2.robjects.r
-    r['source']('init.R')
-
-    global query_dbplyr, query_dplyr, query_sql
-    query_dbplyr = rpy2.robjects.globalenv['query_dbplyr']
-    query_dplyr = rpy2.robjects.globalenv['query_dplyr']
-    query_sql = rpy2.robjects.globalenv['query_sql']
-
-    return rpy2.robjects.globalenv['setup_sqlite'](db)
-
-
-def teardown_r(con):
-    import rpy2.robjects
-    rpy2.robjects.globalenv['teardown_sqlite'](con)
-
-
-def run_r(con, qid, outdir=None, backend='sqlite', queryfunc=None):
-    import rpy2.robjects
-
-    r = rpy2.robjects.r
-    fn = f'queries/{qid}.R'
-    if not Path(fn).exists():
-        return [], {}
-
-    r['source'](fn)
-    func = rpy2.robjects.globalenv[f'tpc_{qid}']
-
-    sql = query_sql(con, func)[0]
-    out_sql(sql, outdir, f'{qid}-r-{backend}.sql')
-
-    errors = []
-    rows = []
-    t1 = time.time()
-    try:
-        res = queryfunc(con, func)
-        rows = res.to_dict('records')
-        out_jsonl(rows, outdir, f'{qid}-{backend}-r.jsonl')
-    except rpy2.rinterface_lib.embedded.RRuntimeError as e:
-        errors.append(str(e))
-
-    t2 = time.time()
-
-    return rows, dict(elapsed_s=t2-t1,
-                      nrows=len(rows),
-                      errors='\n'.join(errors))
+setup_sqlite = SqliteRunner
+setup_ibis = IbisRunner
+setup_dplyr = RRunner
+setup_dbplyr = RRunner
 
 
 def compare(rows1, rows2):
@@ -232,11 +229,12 @@ def compare(rows1, rows2):
 
 @click.command()
 @click.argument('qids', nargs=-1)
-@click.option('--db', default='tpch.db', help='db to run against')
-@click.option('--outdir', type=click.Path(), default=None, help='')
-@click.option('--backend', default='sqlite', help='ibis backend to use')
-def main(qids, db, outdir, backend):
-    kwargs = dict(outdir=outdir)
+@click.option('-d', '--db', default='tpch.db', help='connection string for db to run queries against')
+@click.option('-b', '--backend', default='sqlite', help='backend to use with given db')
+@click.option('-i', '--interface', 'interfaces', multiple=True, help='interface to use with backend: sqlite|ibis|dplyr|dbplyr')
+@click.option('-o', '--output', 'outdir', type=click.Path(), default=None, help='directory to save intermediate and debug outputs')
+@click.option('-v', '--verbose', count=True, help='display more information on stdout')
+def main(qids, db, outdir, interfaces, backend, verbose):
     if outdir:
         os.makedirs(outdir, exist_ok=True)
         try:
@@ -251,41 +249,58 @@ def main(qids, db, outdir, backend):
     if not qids:
         qids = sorted(list(set(Path(fn).stem for fn in glob.glob('queries/*.sql') if '.' in fn)))
 
-    con1 = setup_sqlite(db)
-    con2 = setup_ibis(db, backend=backend)
-    con3 = setup_r(db, backend=backend)
+    if not interfaces:
+        interfaces = ['sqlite', 'ibis', 'dplyr', 'dbplyr']
+
+    runners = [globals()['setup_'+interface](interface=interface, backend=backend) for interface in interfaces]
 
     for qid in qids:
-        rows1, info = run_sqlite(con1, qid, backend='raw-sqlite', **kwargs)
-        out_benchmark(outdir, qid=qid, method='raw-sqlite',
-                      ndiffs=0, **info)
+        results = []
+        for runner, interface in zip(runners, interfaces):
+            runner.setup(db)
 
-        try:
-            rows2, info = run_ibis(con2, qid, backend='ibis-sqlite', **kwargs)
-            diffs2 = compare(rows1, rows2)
-            out_txt('\n'.join(diffs2), outdir, f'{qid}-ibis-diffs.txt')
-        except Exception as e:
-            rows2 = []
-            diffs2 = []
-            info = {'errors': str(e)}
+            info = dict(qid=qid)
+            info.update(runner.info())
 
-        out_benchmark(outdir, qid=qid, method='ibis-sqlite', ndiffs=len(diffs2), **info)
+            try:
+                rows, elapsed_s = runner.run(qid, backend=backend, outdir=outdir)
+                out_jsonl(rows, outdir, f'{qid}-{interface}-{backend}-results.jsonl')
 
-        rows3, info = run_r(con3, qid, queryfunc=query_dplyr, backend='dplyr', **kwargs)
-        diffs3 = compare(rows1, rows3)
-        out_benchmark(outdir, qid=qid, method='dplyr-sqlite',
-                      ndiffs=len(diffs3), **info)
-        out_txt('\n'.join(diffs3), outdir, f'{qid}-dplyr-diffs.txt')
+                info['nrows'] = len(rows)
+                info['elapsed_s'] = elapsed_s
 
-        rows4, info = run_r(con3, qid, queryfunc=query_dbplyr, backend='dbplyr', **kwargs)
-        diffs4 = compare(rows1, rows4)
-        out_benchmark(outdir, qid=qid, method='dbplyr-sqlite',
-                      ndiffs=len(diffs4), **info)
-        out_txt('\n'.join(diffs4), outdir, f'{qid}-dbplyr-diffs.txt')
+                # first interface is baseline for correctness
+                if results:
+                    diffs = compare(results[0], rows)
+                    out_txt('\n'.join(diffs), outdir, f'{qid}-{interface}-{backend}-diffs.txt')
+                    info['ndiffs'] = len(diffs)
 
-    teardown_sqlite(con1)
-    teardown_ibis(con2)
-    teardown_r(con3)
+                results.append(rows)
+            except KeyboardInterrupt:
+                return
+            except Exception as e:
+                rows = []
+                runner.error(type(e).__name__ + ': ' + str(e))
+
+            if runner.errors:
+                info['errors'] = '; '.join(runner.errors)
+
+            if verbose > 0 and runner.warns:
+                info['warns'] = '; '.join(runner.warns)
+
+            if verbose > 1 and runner.prints:
+                info['prints'] = '; '.join(runner.prints)
+
+            if outdir:
+                with open(Path(outdir)/'benchmarks.txt', mode='a') as fp:
+                    print('  '.join(f'{k}:{fmt(v)}' for k, v in info.items()), file=fp)
+
+                with open(Path(outdir)/'benchmarks.jsonl', mode='a') as fp:
+                    print(json.dumps(info), file=fp)
+
+            print('  '.join(f'{k}:{fmt(v)}' for k, v in info.items()))
+
+            runner.teardown()
 
 
 if __name__ == '__main__':
