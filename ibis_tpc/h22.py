e2e1956b08b6670658678a10892a26dfb2ec2f1c
Saul Pwanson <saul@voltrondata.com>
2022-01-14 11:32:47 -0800

    [h22] country codes must be strings

--- ibis_tpc/h22.py
+++ ibis_tpc/h22.py
@@ -1,4 +1,4 @@
-def tpc_h22(con, COUNTRY_CODES=(13, 31, 23, 29, 30, 18, 17)):
+def tpc_h22(con, COUNTRY_CODES=('13', '31', '23', '29', '30', '18', '17')):
     '''Global Sales Opportunity Query (Q22)
 
     The Global Sales Opportunity Query identifies geographies where there are
@@ -9,22 +9,22 @@ def tpc_h22(con, COUNTRY_CODES=(13, 31, 23, 29, 30, 18, 17)):
 
     q = customer.filter([
         customer.C_ACCTBAL > 0.00,
-        customer.C_PHONE.substr(1,2).isin(COUNTRY_CODES),
+        customer.C_PHONE.substr(0, 2).isin(COUNTRY_CODES),
     ])
-    q = q.group_by().aggregate(avg_bal=customer.C_ACCTBAL.mean())
+    q = q.aggregate(avg_bal=customer.C_ACCTBAL.mean())
 
-#    q2 = orders.filter([orders.O_CUSTKEY == customer.C_CUSTKEY])
     custsale = customer.filter([
-        customer.C_PHONE.substr(1,2).isin(COUNTRY_CODES),
+        customer.C_PHONE.substr(0, 2).isin(COUNTRY_CODES),
         customer.C_ACCTBAL > q.avg_bal,
         ~(orders.O_CUSTKEY == customer.C_CUSTKEY).any()
     ])
     custsale = custsale[
-        customer.C_PHONE.substr(1,2).name('cntrycode'),
+        customer.C_PHONE.substr(0, 2).name('cntrycode'),
         customer.C_ACCTBAL
     ]
 
-    gq = q.group_by(custsale.cntrycode)
-    q = gq.aggregate(totacctbal=custsale.C_ACCTBAL.sum())
+    gq = custsale.group_by(custsale.cntrycode)
+    outerq = gq.aggregate(numcust=custsale.count(),
+                          totacctbal=custsale.C_ACCTBAL.sum())
 
-    return q.sort_by(q.cntrycode)
+    return outerq.sort_by(outerq.cntrycode)
