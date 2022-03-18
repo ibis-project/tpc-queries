d4ba37875404765ee07e6876467c9b1c8a74bd1e
Gil Forsyth <gil@forsyth.dev>
2022-03-18 16:28:08 -0400

    fix(add_date): fix incorrect year increment
    Because we had `y += m // 12`, if the month was december, the year would
    get incremented (even if `dy = dm = dd = 0`)
    
    Using modular arithmetic instead fixes it.

--- ibis_tpc/utils.py
+++ ibis_tpc/utils.py
@@ -1,25 +1,16 @@
 import datetime
 import functools
+from dateutil.relativedelta import relativedelta
 
 
 @functools.singledispatch
 def add_date(datestr, dy=0, dm=0, dd=0):
     dt = datetime.date.fromisoformat(datestr)
-    m = dt.month + dm
-    y = dt.year + dy
-    y += m // 12
-    m = ((m - 1) % 12) + 1
-
-    newdt = datetime.date(y, m, dt.day) + datetime.timedelta(days=dd)
-    return newdt.isoformat()
+    dt += relativedelta(years=dy, months=dm, days=dd)
+    return dt.isoformat()
 
 
 @add_date.register(datetime.date)
 def _(dt, dy=0, dm=0, dd=0):
-    m = dt.month + dm
-    y = dt.year + dy
-    y += m // 12
-    m = ((m - 1) % 12) + 1
-
-    newdt = datetime.date(y, m, dt.day) + datetime.timedelta(days=dd)
-    return newdt
+    dt += relativedelta(years=dy, months=dm, days=dd)
+    return dt
