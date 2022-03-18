f52a34c4746b9b3884662975d90e529ede861216
Gil Forsyth <gil@forsyth.dev>
2022-03-18 15:31:22 -0400

    feat: add singledispatch to add_date util
    Allow for passing in both date strings and `datetime.date` objects

--- ibis_tpc/utils.py
+++ ibis_tpc/utils.py
@@ -1,12 +1,25 @@
 import datetime
+import functools
 
 
+@functools.singledispatch
 def add_date(datestr, dy=0, dm=0, dd=0):
     dt = datetime.date.fromisoformat(datestr)
-    m = dt.month+dm
-    y = dt.year+dy
+    m = dt.month + dm
+    y = dt.year + dy
     y += m // 12
-    m = ((m-1) % 12) + 1
+    m = ((m - 1) % 12) + 1
 
     newdt = datetime.date(y, m, dt.day) + datetime.timedelta(days=dd)
     return newdt.isoformat()
+
+
+@add_date.register(datetime.date)
+def _(dt, dy=0, dm=0, dd=0):
+    m = dt.month + dm
+    y = dt.year + dy
+    y += m // 12
+    m = ((m - 1) % 12) + 1
+
+    newdt = datetime.date(y, m, dt.day) + datetime.timedelta(days=dd)
+    return newdt
