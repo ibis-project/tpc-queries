d3424b9d8f65859204cf3f7f0c1c3f7e5c2b2c2e
Gil Forsyth <gil@forsyth.dev>
2022-03-22 15:28:00 -0400

    test: add in dummy backend to handle passing unbound tables

--- ibis_tpc/tests/conftest.py
+++ ibis_tpc/tests/conftest.py
@@ -1,6 +1,7 @@
 import duckdb
 import ibis
 import pytest
+from ibis.backends.base import BaseBackend
 from ibis.backends.duckdb.parser import parse_type
 from ibis_substrait.compiler.core import SubstraitCompiler
 
@@ -11,9 +12,42 @@ def unbound_from_duckdb(table):
     )
 
 
+class TPCHBackend(BaseBackend):
+    def __init__(self, fname=":memory:", scale_factor=0.1):
+        self.con = duckdb.connect(fname)
+
+        self.con.execute(f"CALL dbgen(sf={scale_factor})")
+
+        _tables = self.con.execute("PRAGMA show_tables").fetchall()
+        _tables = map(lambda x: x[0], _tables)
+
+        self.tables = {
+            table.alias: unbound_from_duckdb(table)
+            for table in map(
+                self.con.table,
+                _tables,
+            )
+        }
+
+    def table(self, table):
+        return self.tables.get(table)
+
+    def current_database(self):
+        ...
+
+    def list_databases(self):
+        ...
+
+    def list_tables(self):
+        ...
+
+    def version(self):
+        return "awesome"
+
+
 @pytest.fixture(scope="module")
 def con():
-    yield duckdb.connect("tpch.ddb")
+    yield TPCHBackend()
 
 
 @pytest.fixture(scope="module")
@@ -21,71 +55,16 @@ def compiler():
     yield SubstraitCompiler()
 
 
-@pytest.fixture(scope="module")
-def part(con):
-    table = con.table("part")
-    yield unbound_from_duckdb(table)
-
-
-@pytest.fixture(scope="module")
-def supplier(con):
-    table = con.table("supplier")
-    yield unbound_from_duckdb(table)
-
-
-@pytest.fixture(scope="module")
-def partsupp(con):
-    table = con.table("partsupp")
-    yield unbound_from_duckdb(table)
-
-
-@pytest.fixture(scope="module")
-def nation(con):
-    table = con.table("nation")
-    yield unbound_from_duckdb(table)
-
-
-@pytest.fixture(scope="module")
-def region(con):
-    table = con.table("region")
-    yield unbound_from_duckdb(table)
-
-
-@pytest.fixture(scope="module")
-def lineitem(con):
-    table = con.table("lineitem")
-    yield unbound_from_duckdb(table)
-
-
-@pytest.fixture(scope="module")
-def customer(con):
-    table = con.table("customer")
-    yield unbound_from_duckdb(table)
-
-
-@pytest.fixture(scope="module")
-def orders(con):
-    table = con.table("orders")
-    yield unbound_from_duckdb(table)
-
-
 def pytest_runtest_call(item):
     """Dynamically add various custom markers."""
-    #nodeid = item.nodeid
 
     for marker in item.iter_markers(name="duckdb_internal_error"):
-        item.add_marker(
-            pytest.mark.xfail(reason="DuckDB Internal Error")
-            )
+        item.add_marker(pytest.mark.xfail(reason="DuckDB Internal Error"))
     for marker in item.iter_markers(name="corr_sub_query"):
         item.add_marker(
             pytest.mark.xfail(reason="Substrait doesn't support correlated subqueries")
-            )
+        )
     for marker in item.iter_markers(name="duckdb_catalog_error"):
-        item.add_marker(
-            pytest.mark.xfail(reason="DuckDB Catalog Error")
-            )
+        item.add_marker(pytest.mark.xfail(reason="DuckDB Catalog Error"))
     for marker in item.iter_markers(name="ibis_substrait_error"):
-        item.add_marker(
-            pytest.mark.xfail(reason="Ibis substrait compilation failure")
-            )
+        item.add_marker(pytest.mark.xfail(reason="Ibis substrait compilation failure"))
