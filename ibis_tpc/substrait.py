da99fa19c0bc6876e7707da781b5677bb42f833f
Gil Forsyth <gil@forsyth.dev>
2022-05-12 17:17:13 -0400

    feat: add substrait runner
    Currently it only runs on `duckdb` -- we can make it extensible later.
    
    Other small changes:
    * A runner returning empty `rows` now raises an error since all queries
      should return _some_ output
    * Moving some substrait helper stuff into a module instead of `conftest.py`

---
+++ ibis_tpc/substrait.py
@@ -0,0 +1,47 @@
+# noqa: D100
+
+import duckdb
+import ibis
+from ibis.backends.base import BaseBackend
+from ibis.backends.duckdb.datatypes import parse_type
+from ibis_substrait.compiler.core import SubstraitCompiler
+
+
+class TPCHBackend(BaseBackend):  # noqa: D101
+    def __init__(self, fname="", scale_factor=0.1):  # noqa: D107
+        self.con = duckdb.connect(fname)
+
+        if not fname:
+            self.con.execute(f"CALL dbgen(sf={scale_factor})")
+
+        _tables = self.con.execute("PRAGMA show_tables").fetchall()
+        _tables = map(lambda x: x[0], _tables)
+
+        self.tables = {
+            table.alias: unbound_from_duckdb(table)
+            for table in map(
+                self.con.table,
+                _tables,
+            )
+        }
+
+    def table(self, table):  # noqa: D102
+        return self.tables.get(table)
+
+    def current_database(self):  # noqa: D102
+        ...
+
+    def list_databases(self):  # noqa: D102
+        ...
+
+    def list_tables(self):  # noqa: D102
+        ...
+
+    def version(self):  # noqa: D102
+        return "awesome"
+
+
+def unbound_from_duckdb(table):  # noqa: D103
+    return ibis.table(
+        list(zip(table.columns, map(parse_type, table.dtypes))), name=table.alias
+    )
--- ibis_tpc/tests/conftest.py
+++ ibis_tpc/tests/conftest.py
@@ -1,48 +1,6 @@
-import duckdb
-import ibis
 import pytest
-from ibis.backends.base import BaseBackend
-from ibis.backends.duckdb.datatypes import parse_type
 from ibis_substrait.compiler.core import SubstraitCompiler
-
-
-def unbound_from_duckdb(table):
-    return ibis.table(
-        list(zip(table.columns, map(parse_type, table.dtypes))), name=table.alias
-    )
-
-
-class TPCHBackend(BaseBackend):
-    def __init__(self, fname=":memory:", scale_factor=0.1):
-        self.con = duckdb.connect(fname)
-
-        self.con.execute(f"CALL dbgen(sf={scale_factor})")
-
-        _tables = self.con.execute("PRAGMA show_tables").fetchall()
-        _tables = map(lambda x: x[0], _tables)
-
-        self.tables = {
-            table.alias: unbound_from_duckdb(table)
-            for table in map(
-                self.con.table,
-                _tables,
-            )
-        }
-
-    def table(self, table):
-        return self.tables.get(table)
-
-    def current_database(self):
-        ...
-
-    def list_databases(self):
-        ...
-
-    def list_tables(self):
-        ...
-
-    def version(self):
-        return "awesome"
+from ibis_tpc.substrait import TPCHBackend
 
 
 @pytest.fixture(scope="module")
