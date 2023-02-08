503f19de295881e1f2b58afd0c777c7e9f55b1a9
Gil Forsyth <gil@forsyth.dev>
2023-02-08 10:39:25 -0500

    fix(substrait): deal with decimal/date outputs from duckdb substrait

--- ibis_tpc/substrait.py
+++ ibis_tpc/substrait.py
@@ -3,33 +3,24 @@
 import duckdb
 import ibis
 from ibis.backends.base import BaseBackend
-from ibis_substrait.compiler.core import SubstraitCompiler
-
-try:
-    from ibis.backends.duckdb.datatypes import parse_type
-except ImportError:
-    from ibis.backends.duckdb.datatypes import parse as parse_type
 
 
 class TPCHBackend(BaseBackend):  # noqa: D101
     def __init__(self, fname="", scale_factor=0.1):  # noqa: D107
-        self.con = duckdb.connect(fname)
-        self.con.install_extension("substrait")
-        self.con.load_extension("substrait")
+        if fname:
+            con = ibis.duckdb.connect(fname)
+        else:
+            con = ibis.duckdb.connect()
 
         if not fname:
-            self.con.execute(f"CALL dbgen(sf={scale_factor})")
-
-        _tables = self.con.execute("PRAGMA show_tables").fetchall()
-        _tables = map(lambda x: x[0], _tables)
+            con.raw_sql(f"CALL dbgen(sf={scale_factor})")
 
         self.tables = {
-            table.alias: unbound_from_duckdb(table)
-            for table in map(
-                self.con.table,
-                _tables,
-            )
+            name: con.tables.get(name).unbind() for name in con.list_tables()
         }
+        del con
+
+        self.con = duckdb.connect(fname)
 
     def table(self, table):  # noqa: D102
         return self.tables.get(table)
@@ -45,9 +36,3 @@ class TPCHBackend(BaseBackend):  # noqa: D101
 
     def version(self):  # noqa: D102
         return "awesome"
-
-
-def unbound_from_duckdb(table):  # noqa: D103
-    return ibis.table(
-        list(zip(table.columns, map(parse_type, table.dtypes))), name=table.alias
-    )
