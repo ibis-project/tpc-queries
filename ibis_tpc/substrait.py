b9e77e552a1dae02859e1f69106797c7f34d10a2
Krisztián Szűcs <szucs.krisztian@gmail.com>
2023-03-28 16:34:29 +0200

    chore: add a fake implementation for abstractmethod `create_view`

--- ibis_tpc/substrait.py
+++ ibis_tpc/substrait.py
@@ -28,7 +28,10 @@ class TPCHBackend(BaseBackend):  # noqa: D101
     def table(self, table):  # noqa: D102
         return self.tables.get(table)
 
-    def create_table(self, name):  # noqa: D102
+    def create_table(self, *args, **kwargs):  # noqa: D102
+        ...
+
+    def create_view(self, *args, **kwargs):  # noqa: D102
         ...
 
     def current_database(self):  # noqa: D102
